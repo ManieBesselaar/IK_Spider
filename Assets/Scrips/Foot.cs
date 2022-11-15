c54b0eeed2d1c18dcf63defb301c5114561dd5f6
Manie Besselaar <manie.besselaar@gmail.com>
2022-11-15 14:40:00 +0400

    Fixed raycasting and added target position advance

--- Assets/Scrips/Foot.cs
+++ Assets/Scrips/Foot.cs
@@ -21,10 +21,22 @@ public class Foot : MonoBehaviour
     float _postitionTolerance = .1f; //This avoids the problem of the foot target never
                                      //reaching it's destination due to a small difference in precision. Just get close enough
     [SerializeField][Tooltip("How high the foot should be lifted with each step.")] float _stepHeight = .5f;
+    [SerializeField]
+    [Tooltip("The transform of the spider body to cast the ray from, specifically the parent of the footPlacementTargets")]
+    Transform _bodyTransform;
+    [SerializeField]
+    [Tooltip("How far down the terrain detection ray should be cast")] float _raycastDepth = 2;
+    [SerializeField] LayerMask _terrainLayer;
+    [SerializeField]
+    [Tooltip("A contact filter to filter out hits that are not terrain")]
+    ContactFilter2D _terraingFilter;
     Vector3 _nextTarget; //Where the foot target is heading to.
-    Vector3 _debug_halfway_point = Vector3.zero; // Used to store the position for the halfway target, for drawing debug gizmos
+    Vector3 halfway_point = Vector3.zero; // Used to store the position for the halfway target, for drawing debug gizmos
+    Vector3 _plantingPoint = Vector3.zero;
     public bool isPlanted = false; // True is the opposing foot is in a planted state. Helps prevent having all feet in the air at once
-    [SerializeField][Tooltip("The foot that should be planted while this foot is in the air")] Foot _opposingFoot; 
+    [SerializeField][Tooltip("The foot that should be planted while this foot is in the air")] Foot _opposingFoot;
+    [SerializeField][Tooltip("Set true to have leg work without an opposing foot")] bool isSoloLeg = false;
+
     // Start is called before the first frame update
     enum StepState
     {
@@ -36,19 +48,21 @@ public class Foot : MonoBehaviour
     void Start()
     {
         _nextTarget = _footPlacementTarget.position; // start planting your foot so you are ready to move.
-       
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_state == StepState.planted && _opposingFoot.isPlanted)
+        if (_state == StepState.planted && ( _opposingFoot.isPlanted || isSoloLeg))
         {
 
             if (Vector3.Distance(transform.position, _footPlacementTarget.position) > _stepSize) //If this foot is at its plant position
             {
+                _plantingPoint = GetTerrain(_footPlacementTarget.position);
                 _nextTarget = CalculateLiftPos(); // Calculate where the foot should be at the top of the lift
-                _debug_halfway_point =_nextTarget; // Assign for debugging. TODO: remove before release
+                halfway_point =_nextTarget; // Assign for debugging. TODO: remove before release
                 _state = StepState.lifting;
                
                 isPlanted = false; //Started lifting the foot and it is no longer on the ground.
@@ -61,8 +75,8 @@ public class Foot : MonoBehaviour
             if (Vector3.Distance(transform.position, _nextTarget) < _postitionTolerance) 
             {
                 //Set the target to the final placement position and change the state accordingly
-                _nextTarget = _footPlacementTarget.position;
 
+                _nextTarget = _plantingPoint;
                 _state = StepState.moving_to_plant;
                 Debug.Log("State changed " + _state);
             }
@@ -87,12 +101,12 @@ public class Foot : MonoBehaviour
     /// <summary>
     /// Calculate the position where the foot will be at the top of it's arc in the step cycle.
     /// </summary>
-    /// <returns></returns>
+    /// <returns>Returns a vector3 posittion for the top of the step cycle</returns>
     private Vector3 CalculateLiftPos()
     {
         //Get a straight line vector between the current position of this foot and the position of it's _footPlacementTarget's
  //position , then divide by 2 and add to the current position to get a point halfway to your destination.
-        Vector3 halfwayPoint = transform.position +( _footPlacementTarget.position - transform.position ) /2;
+        Vector3 halfwayPoint = transform.position +( _plantingPoint - transform.position ) /2;
         
         /* 
          Calculate how much the halfway point needs to shift to be at the step height perpendicular to the line between the current and target
@@ -109,11 +123,21 @@ public class Foot : MonoBehaviour
     {
         //Draw a blue sphere where the calculated halfway point is for debugging
         Gizmos.color = Color.blue;
-        Gizmos.DrawSphere(_debug_halfway_point, .1f);
+        Gizmos.DrawSphere(halfway_point, .1f);
 
         //Draw a yellow sphere at the point where the foot is currently heading
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(_nextTarget,.3f);
     }
-   
+   Vector3 GetTerrain( Vector3 targetedPosition)
+    {
+       
+        RaycastHit2D _terrainHit = Physics2D.Raycast(_bodyTransform.position, _footPlacementTarget.position - _bodyTransform.position, _raycastDepth);
+        Debug.DrawRay(_bodyTransform.position, _footPlacementTarget.position - _bodyTransform.position, Color.white, 1f);
+       Debug.Log("Terrain Hit result point " + _terrainHit.point + " layer name is " + _terrainLayer.ToString());
+       
+        if (_terrainHit.point == Vector2.zero) return targetedPosition; // If the hit does not register any hits then just return the targeted pos
+        
+        return _terrainHit.point;
+    }
 }
--- Assets/Scrips/Movement.cs
+++ Assets/Scrips/Movement.cs
@@ -7,11 +7,15 @@ public class Movement : MonoBehaviour
 {
    
     [SerializeField][Tooltip("How fast the spider moves to it's left and right")] float _movementSpeed = 5f;
+    [SerializeField]
+    [Tooltip("How far ahead should the foot placement targets be advanced when moving")] float _footPlacementTargetAdcance = 1f;
+    [SerializeField] Transform _footPlacementTargetsParent;
     float _currentInput = 0;
+    Vector2 _placementTargetsInitialPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _placementTargetsInitialPos = _footPlacementTargetsParent.localPosition;
     }
 
     // Update is called once per frame
@@ -21,7 +25,8 @@ public class Movement : MonoBehaviour
         //Current unput will have a value between -1 and 1, negative values indicat a left movement and positi
       if(_currentInput != 0) //Only move if there is a control input
         {
-            
+            //Shift the footPlacement targets to left or right depending on walk direction and rate
+            _footPlacementTargetsParent.localPosition =_placementTargetsInitialPos + (_currentInput * _footPlacementTargetAdcance * Vector2.right);  
             transform.position += _currentInput * _movementSpeed * Time.deltaTime * transform.right;
         }
      
